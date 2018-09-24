b0dfce62f80c0a81b5d232e332de5719ed67eb4a
Daniel Lim <dlim87@gmail.com>
2018-09-24 14:12:17 -0700

    created project

---
+++ src/Components/square.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './board.css'
+
+class Square extends Component {
+
+updateSquare(e){
+  let X=this.props.location[0]
+  let Y=this.props.location[1]
+  console.log(this.props.location)
+  this.props.updateValue(X,Y)
+}
+
+  render() {
+    return (
+      <div className="Squares" onClick={this.updateSquare.bind(this)} >
+      <p className="squareContent">
+      {this.props.contents}</p>
+      </div>
+    );
+  }
+}
+
+export default Square;
