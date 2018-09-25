9eae5fb93cdf026c6d44308d56f8a689fe8d8096
Daniel Lim <dlim87@gmail.com>
2018-09-25 16:52:41 -0700

    added board

--- src/Components/square.js
+++ src/Components/square.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
-import './board.css'
+import '../css/square.css'
 
 class Square extends Component {
 
-updateSquare(e){
+updateSquare=(e)=>{
+  // console.log(this.props.location)
   let X=this.props.location[0]
   let Y=this.props.location[1]
-  console.log(this.props.location)
+  // console.log(this.props.location)
   this.props.updateValue(X,Y)
 }
 
   render() {
+    // console.log(this.props.location);
     return (
-      <div className="Squares" onClick={this.updateSquare.bind(this)} >
-      <p className="squareContent">
-      {this.props.contents}</p>
+      <div className="Squares" onClick={this.updateSquare} >
+      {this.props.contents}
       </div>
     );
   }
---
+++ src/Pages/Board.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react'
+import '../css/board.css'
+import Square from '../Components/square'
+
+class Board extends Component {
+  constructor(props){
+    super(props)
+    this.state= {
+      boardArray: [...Array(10)].map(e=>Array(10)),
+      boardLocation: []
+    }
+  }
+
+  generateCoords = (array)=>{
+    let coords=JSON.parse(JSON.stringify(array))
+    coords.map((e,i)=>coords.map((e,j)=> coords[i][j]=[i,j]))
+    this.setState({boardLocation:coords})
+  }
+  componentWillMount(){
+  this.generateCoords(this.state.boardArray)
+}
+  render(){
+    console.log(this.state.boardArray);
+    console.log(this.state.boardLocation);
+
+    let squares = this.state.boardArray.map((square, i)=>{return (
+      this.state.boardArray.map((square,j)=>{
+        // console.log(this.state.boardLocation[i][j])
+        return(
+        <Square key={JSON.stringify(this.state.boardLocation[i][j])} updateValue={this.updateValue} contents={this.state.boardArray[i][j]} location={this.state.boardLocation[i][j]} />
+      )})
+    )})
+
+    return (
+      <div className="board">
+      {squares}
+      </div>
+    )
+  }
+
+  updateValue = (row,col) => {
+   console.log(`click! ${row},${col}`)
+   let hiticon="X"
+   let missicon="0"
+   let temparray=this.state.boardArray.slice()
+
+   if(temparray[row][col]) alert("You already guessed there")
+   else{
+     temparray[row][col]=hiticon
+     this.setState({boardArray:temparray})
+   }
+ }
+
+}
+
+export default Board
